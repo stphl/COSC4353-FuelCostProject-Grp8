afe4fdc9626a81a2c82520e453577abe30ce61e1
Stephany Lopez <slopez44@uh.edu>
2023-10-11 13:28:27 -0500

    Updated profile endpoints

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override') //unused for now, login feature coming next
+const url = require('url') // Require url object to help handling redirects passing parameters
 
 //sets up passport, which handles everything related to logins
 const initializePassport = require('./passport-config')
@@ -29,6 +30,8 @@ temp_users.push(
         uname: 'testing_user',
         psw: '$2b$10$wOSuTBXWn93giJEkbsvrfOlYQwn8R3dEPBWws4r7u7I8M0NY7186O' //this is 'test' but hashed
       })
+
+var temp_users_profile = []
 
 //allows us to use assets, namely CSS
 app.use(express.static("public"));
@@ -96,7 +99,90 @@ app.get('/history',checkAuthenticated,(req, res) =>{
 })
 
 app.get('/profile',checkAuthenticated,(req, res) =>{
-    res.render('profile.ejs')
+    render_data = {
+        error_message: req.query.error_message,
+        success_message: req.query.success_message
+    }
+    res.render('profile.ejs', render_data)
+})
+
+app.post('/profile', checkAuthenticated,async (req,res)=>{
+    var isValid = true
+    var errorMessage = []
+
+    if (req.body.fullname == null || req.body.fullname.length == 0 || req.body.fullname === undefined) {
+        isValid = false
+        errorMessage.push("Please enter full name")
+    }
+    if (req.body.fullname.length > 50) {
+        isValid = false
+        errorMessage.push("Full name is too long")
+    }
+    if (req.body.address1 == null || req.body.address1.length == 0 || req.body.address1 === undefined) {
+        isValid = false
+        errorMessage.push("Please enter address")
+    }
+    if (req.body.address1.length > 100) {
+        isValid = false
+        errorMessage.push("Address1 is too long")
+    }
+    if (req.body.address2.length > 100) {
+        isValid = false
+        errorMessage.push("Address2 is too long")
+    }
+    if (req.body.city == null || req.body.city.length == 0 || req.body.city === undefined) {
+        isValid = false
+        errorMessage.push("Please enter city")
+    }
+    if (req.body.city.length > 100) {
+        isValid = false
+        errorMessage.push("City name is too long")
+    }
+    if (req.body.state == null || req.body.state.length == 0 || req.body.state === undefined) {
+        isValid = false
+        errorMessage.push("Please select a state")
+    }
+    if (req.body.zipcode == null || req.body.zipcode.length == 0 || req.body.zipcode === undefined) {
+        isValid = false
+        errorMessage.push("Please enter a zipcode")
+    }
+    if (req.body.zipcode.lenght < 5 || req.body.zipcode.lenght > 9) {
+        isValid = false
+        errorMessage.push("Zipcode lenght invalid")
+    }
+    if (isValid === true) {
+        for (let i = 0; i < temp_users.length; i++) {
+            if (temp_users[i].id == req.session.passport.user) {
+                temp_users[i]["fullname"] = req.body.fullname
+                temp_users[i]["address1"] = req.body.address1
+                temp_users[i]["address2"] = req.body.address2
+                temp_users[i]["city"] = req.body.city
+                temp_users[i]["zipcode"] = req.body.zipcode
+            }
+        }
+        // Redirect to profile
+        res.redirect(url.format({
+            pathname: "/profile",
+            query: {
+                "error_message": errorMessage.join(", "),
+                "success_message": "Information saved successfully!"
+            }
+        }))
+    } else {
+        console.log("Entries are not valid! Form loaded again")
+
+        // Redirect to profile
+        res.redirect(url.format({
+            pathname: "/profile",
+            query: {
+                "error_message": errorMessage.join(", "),
+                "success_message": ""
+            }
+        }))
+    }
+    console.log(req.session.passport.user)
+    console.log(req.body)
+    console.log(temp_users)
 })
 
 
@@ -118,7 +204,6 @@ function checkAuthenticated(req, res, next) {
     next()
   }
   
-
 
 app.listen(3000)
 

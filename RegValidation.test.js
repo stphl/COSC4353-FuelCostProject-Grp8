ef82e043579b61e009f7f8534c1c19cce2c8076c
FlavoredLime <64186283+FlavoredLime@users.noreply.github.com>
2023-10-11 16:03:34 -0500

    Add jest to packages and add registration validator tests

---
+++ RegValidation.test.js
@@ -0,0 +1,53 @@
+const validator = require('./RegValidation.js')
+
+describe(
+    'Tests for Registration Validation',() =>{
+
+        test('Test 1 for lower upper requirement', () =>{
+            expect(
+                validator.CheckLowUp("COSC4000")
+            ).toBeFalsy()
+        })
+
+        test('Test 2 for lower upper requirement', () =>{
+            expect(
+                validator.CheckLowUp("WorkingPassword")
+            ).toBeTruthy()
+        })
+
+        test('Test 1 for length requirement', () =>{
+            expect(
+                validator.CheckLength("WorkingPassword")
+            ).toBeTruthy()
+        })
+        test('Test 2 for length requirement', () =>{
+            expect(
+                validator.CheckLength("short!")
+            ).toBeFalsy()
+        })
+
+        test('Test 1 for symbol requirement', () =>{
+            expect(
+                validator.CheckSymb("WorkingPassword!")
+            ).toBeTruthy()
+        })
+        test('Test 2 for  symbol requirement', () =>{
+            expect(
+                validator.CheckSymb("nosymbol")
+            ).toBeFalsy()
+        })
+
+        test('Test 1 for check requirement', () =>{
+            expect(
+                validator.CheckMatch("WorkingPassword!","WorkingPassword!")
+            ).toBeTruthy()
+        })
+        test('Test 2 for  check requirement', () =>{
+            expect(
+                validator.CheckLength("not","equal")
+            ).toBeFalsy()
+        })
+
+
+}
+)

fff685a4a9c99733e4a6acc48b029102fb8414a6
BlazeX15 <jsbahga@hotmail.com>
2023-10-27 10:14:41 -0500

    Made changes to the schema

--- createtables.js
+++ createtables.js
@@ -7,9 +7,14 @@ const db = new sqlite3.Database("./database.db", sqlite3.OPEN_READWRITE, (err)=>
 //turns on foreign key support for sqlite3
 db.run("PRAGMA foreign_keys = ON;")
 
+//db.run("DROP TABLE UserCredentials");
+//db.run("DROP TABLE ClientInformation");
+
+
 statement_usercreds = `CREATE TABLE UserCredentials
                             (id INTEGER PRIMARY KEY,
-                            password  CHAR(60) NOT NULL)`
+                            username Char(15) NOT NULL UNIQUE,
+                            password  CHAR(60) NOT NULL)`;
 db.run(statement_usercreds);
 
 statement_states = `CREATE TABLE states
@@ -17,18 +22,17 @@ statement_states = `CREATE TABLE states
                         state CHAR(100) PRIMARY KEY,
                         abbreviation CHAR(2) UNIQUE NOT NULL
                         );`
-db.run(statement_states);
+//db.run(statement_states);
 statement_client_info = `CREATE TABLE ClientInformation (
                             id INTEGER PRIMARY KEY AUTOINCREMENT,
                             user_id INTEGER REFERENCES UserCredentials(id),
-                            username CHAR(15) NOT NULL UNIQUE,
                             fullname CHAR(100) NOT NULL,
                             address1 CHAR(100) NOT NULL,
                             address2 CHAR(100),
                             city CHAR(100) NOT NULL,
                             state_id char(100) REFERENCES states(state),
                             zipcode INTEGER NOT NULL
-                            );`
+                            )`;
 db.run(statement_client_info);
 
 statement_fuelquote = `CREATE TABLE FuelQuote
@@ -47,4 +51,4 @@ statement_fuelquote = `CREATE TABLE FuelQuote
                                 );`
 
 
-db.run(statement_fuelquote);
+//db.run(statement_fuelquote);

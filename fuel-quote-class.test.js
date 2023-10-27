02d7c25cf129863365ea51ad9326776ce51d87bd
FlavoredLime <64186283+FlavoredLime@users.noreply.github.com>
2023-10-27 01:19:44 -0500

    Edit fuel quote tests to include new fields

--- fuel-quote-class.test.js
+++ fuel-quote-class.test.js
@@ -3,21 +3,21 @@ const FuelQuote = require('./fuel-quote-class.js').FuelQuote
 describe("Tests for Fuel Quote Class",()=>{
 
     test("In state cost for 100 gals for new customer",() =>{
-        quote = new FuelQuote("123 Testing Ln", "Houston", "TX", 100,"01/02/34")
+        quote = new FuelQuote("1","123 Testing Ln", "Houston", "TX",77, 100,"01/02/34")
         quote.calcTotalPrice(0)
         expect(
             quote.total_price
         ).toBe(228)
     })
     test("In state cost for 1000 gals for new customer",() =>{
-        quote = new FuelQuote("123 Testing Ln", "Houston", "TX", 1000,"01/02/34")
+        quote = new FuelQuote("1","123 Testing Ln", "Houston", "TX",77, 1000,"01/02/34")
         quote.calcTotalPrice(0)
         expect(
             quote.total_price
         ).toBe(2200)
     })
     test("In state cost for 1000 gals for returning customer",() =>{
-        quote = new FuelQuote("123 Testing Ln", "Houston", "TX", 1000,"01/02/34")
+        quote = new FuelQuote("1","123 Testing Ln", "Houston", "TX",77, 1000,"01/02/34")
         quote.calcTotalPrice([1])
         expect(
             quote.total_price
@@ -25,7 +25,7 @@ describe("Tests for Fuel Quote Class",()=>{
     })
 
     test("Out of state cost for 500 gals for returning customer",() =>{
-        quote = new FuelQuote("123 Testing Ln", "Houston", "FL", 500,"01/02/34")
+        quote = new FuelQuote("1","123 Testing Ln", "Houston", "FL",77, 500,"01/02/34")
         quote.calcTotalPrice([1])
         expect(
             quote.total_price
